c4d3d6c26d37bef9ac3ae875b3294326f4594507
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-03 02:34:41 +0000

    created a new script named 'task_6.sql'

---
+++ task_6.sql
@@ -0,0 +1,10 @@
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
