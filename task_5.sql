95bb5b54536beba8c5c4acb82569d3edaa05baeb
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-03 02:16:57 +0000

    created a new script named 'task_5.sql'

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+SELECT * FROM Customers;
