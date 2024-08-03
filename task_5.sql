9805db9f1db7a2a9caa80265031adfe21d207db0
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-03 02:23:56 +0000

    changes to the  script named 'task_5.sql'

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,12 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+RENAME TABLE CuStomers TO customer;
+
+DELETE FROM customer
+WHERE customer_id = 1;
+
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
-SELECT * FROM Customers;
+SELECT * FROM customer;
