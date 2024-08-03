211699e8ebcce6f3236519db927d855e280d4f7a
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-03 02:38:00 +0000

    Changes to be committed: modified:   task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,12 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
 SELECT * FROM customer;
